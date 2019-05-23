03d0631de10e6f603ac5df95d2773327cc831b8c
djansyle <djansyledjans@gmail.com>
2019-05-23 15:18:21 +0800

    feat(lock): add locking of request and releasing

--- src/idempotent.ts
+++ src/idempotent.ts
@@ -1,12 +1,24 @@
 export interface IdempotentStore {
   get<T = any>(request: string): Promise<{ found: boolean; result: T }>;
   set<T>(request: string, response: T): Promise<boolean>;
+  lock(request: string): Promise<boolean>;
+  release(request: string): Promise<boolean>;
+}
+
+export class LockError extends Error {
+  constructor() {
+    super('Failed to lock the request id');
+  }
 }
 
 export class Idempotent {
   constructor(private store: IdempotentStore) {}
 
   async execute<T = any>(fn: () => Promise<T>, request: string): Promise<T> {
+    const lock = await this.store.lock(request);
+    if (!lock) {
+      throw new LockError();
+    }
     const response = await this.store.get(request);
     if (response && response.found) {
       return response.result;
@@ -14,6 +26,8 @@ export class Idempotent {
 
     let res = await fn();
     await this.store.set(request, res);
+
+    await this.store.release(request);
     return res;
   }
 }
